c7e3085aabe1a2fd9225398f18812c73b9732db2
HirushaRukmal <hirusharukmal266@gmail.com>
2022-05-27 18:20:01 +0530

    Implemented the Backend of Admin

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
 // import routes
 const groupRoute = require('./routes/Group');
 const studentRoute = require('./routes/Student');
 const SubmissionRoute = require('./routes/Admin_st');
+const adminRoute = require('./routes/uprofile');
 
 // App
 const app = express();
 
 // Database
 mongoose
-    .connect(process.env.DATABASE, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("DB Connected"))
-    .catch((err) => console.log(err));
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
 
 // Middlewares
 app.use(cors());
@@ -39,6 +39,7 @@ app.use(bodyParser.json());
 app.use('/group', groupRoute);
 app.use('/student', studentRoute);
 app.use('/admin', SubmissionRoute);
+app.use('/admin', adminRoute);
 
 // Post
 const port = process.env.PORT || 8000;
