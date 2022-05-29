e9b9776374c042f809f4043fb33d04445fb117a7
VisharaPrabuddhi <86484671+VisharaPrabuddhi@users.noreply.github.com>
2022-05-30 02:35:42 +0530

    supervisor

---
+++ models/SupervisorTopics.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const SupervisorTopicsSchema = new mongoose.Schema({
+
+    supervisorId: {
+        type: String,
+        required: true
+    },
+    supervisorName: {
+        type: String,
+        required: true
+    },
+    groupId: {
+        type: String,
+        required: true
+    },
+    groupName: {
+        type: String,
+        required: true
+    },
+    topicId: {
+        type: String,
+        required: true
+    },
+    topic: {
+        type: String,
+        required: true
+    },
+    topicCategory: {
+        type: String,
+        required: true
+    },
+    topicSubmission: {
+        type: String,
+        required: true
+    }, // links docs regarding selected topic
+    topicStatus: {
+        type: String,
+        default: "Pending"
+    }, // accept or reject
+    teamMembers: {
+        type: String,
+        required: true
+    },
+    regNumbers: {
+        type: String,
+        required: true
+    },
+    studentEmail: {
+        type: String,
+        required: true
+    }, // send accept / reject message
+    messageId: {
+        type: String,
+        required: true
+    },
+    supervisorMessage: {
+        type: String,
+        required: true
+    },
+    studentMessage: {
+        type: String,
+        required: true
+    },
+    messageStatus: {
+        type: String,
+        required: true
+    },
+    
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('SupervisorTopics', SupervisorTopicsSchema);
